2dd9c9eba1407cea05a5238a41793a59bb0b7b5c
ItaloSoares01 <127902962+ItaloSoares01@users.noreply.github.com>
2023-05-16 20:03:14 -0300

    att

---
+++ Struct/Struct.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+
+struct Aluno // dado chamado aluno com variaveis diferentes;
+{
+  char nome[20];
+  char telefone[20];
+  char cpf[11];
+  double todoDinheiroDisponivel;
+  int idade
+};
+
+struct Professor {
+
+char nome[20];
+  char telefone[20];
+  char cpf[11];
+  double todoDinheiroDisponivelParaMim;
+
+};
+
+
+int main(){
+
+
+struct Aluno aluno1;
+struct Professor professor1;
+
+printf("Digite o nome do Aluno: ");
+  scanf("%d", &aluno1.nome);
+printf("Digite o valor disponivel: ");
+
+
+
+
+}
