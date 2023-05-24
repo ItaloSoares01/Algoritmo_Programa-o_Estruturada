10f89c91ee49e35b786be512f889b2f8b55d7fa3
ItaloSoares01 <127902962+ItaloSoares01@users.noreply.github.com>
2023-05-23 22:09:32 -0300

    inclua até 1000 usuarios

--- Projeto_Vetor_Funcao/projeto_final.c
+++ Projeto_Vetor_Funcao/projeto_final.c
@@ -20,3 +20,62 @@ int numUsuarios = 0;
 int Id_usuario() {
     return rand() % 1000 - 0.44;
 }
+
+
+void adicionarUsuario() {
+    if (numUsuarios >= MAXIMO_DE_USUARIOS) {
+        printf("Quantidade maxima de usuarios atingida.\n");
+        return;
+    }
+
+    int newId = Id_usuario();
+    ids[numUsuarios] = newId;
+
+
+
+    printf("Digite o nome completo: ");
+    fgets(nomes[numUsuarios], sizeof(nomes[numUsuarios]), stdin);
+    nomes[numUsuarios][strcspn(nomes[numUsuarios], "\n")] = '\0';
+
+    printf("Digite o email: ");
+    fgets(emails[numUsuarios], sizeof(emails[numUsuarios]), stdin);
+    emails[numUsuarios][strcspn(emails[numUsuarios], "\n")] = '\0';
+
+    while (!validacaoEmail(emails[numUsuarios])) {
+        printf("Email Inconrreto. Tente novamente: ");
+        fgets(emails[numUsuarios], sizeof(emails[numUsuarios]), stdin);
+        emails[numUsuarios][strcspn(emails[numUsuarios], "\n")] = '\0';
+    }
+
+    printf("Digite o sexo (Feminino, Masculino ou Nao Definido): ");
+    fgets(generos[numUsuarios], sizeof(generos[numUsuarios]), stdin);
+    generos[numUsuarios][strcspn(generos[numUsuarios], "\n")] = '\0';
+
+    while (!validacaoGen(generos[numUsuarios])) {
+        printf("Sexo nao cadastrado. Tente novamente: ");
+        fgets(generos[numUsuarios], sizeof(generos[numUsuarios]), stdin);
+        generos[numUsuarios][strcspn(generos[numUsuarios], "\n")] = '\0';
+    }
+
+    printf("Digite seu endereco: ");
+    fgets(enderecos[numUsuarios], sizeof(enderecos[numUsuarios]), stdin);
+    enderecos[numUsuarios][strcspn(enderecos[numUsuarios], "\n")] = '\0';
+
+    printf("Digite a altura (entre 1 e 2 metros): ");
+    scanf("%lf", &alturas[numUsuarios]);
+
+    while (!validacaoAltura(alturas[numUsuarios])) {
+        printf("Altura Incorreta. Digite novamente: ");
+        scanf("%lf", &alturas[numUsuarios]);
+    }
+
+       printf("Digite 1 se o usuario foi vacinado ou 0 caso nao cadastrado:");
+    scanf("%d", &vacinados[numUsuarios]);
+    printf("\n\n");
+    printf("\tO usuario foi cadastrado com sucesso.\n");
+    numUsuarios++;
+
+
+    printf("\t(Seja bem vindo usuario(ID): %d\n\n)", newId);
+    
+}
